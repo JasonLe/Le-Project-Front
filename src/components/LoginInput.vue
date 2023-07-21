955e8ad0a0e04cdae630f426197d2a0013dad735
le <2461251662@qq.com>
2023-07-21 14:16:42 +0800

    init

---
+++ src/components/LoginInput.vue
@@ -0,0 +1,65 @@
+<template>
+  <div >
+    <el-form :model="form" label-width="120px" class="main-container">
+      <el-form-item label="Activity name">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="Activity zone">
+        <el-select v-model="form.region" placeholder="please select your zone">
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Resources">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="Sponsor" />
+          <el-radio label="Venue" />
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="Activity form">
+        <el-input v-model="form.desc" type="textarea" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Create</el-button>
+        <el-button>Cancel</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+  
+<script>
+import { reactive } from 'vue'
+export default {
+  name: 'LoginInput',
+  data() {
+    return {
+      form: reactive({
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: '',
+      })
+    }
+  },
+  methods: {
+    onSubmit: function () {
+      alert("onSubmit")
+    }
+  }
+}
+</script>
+  
+<style scoped>
+.main-container {
+  text-align: center;
+
+}
+el-form-item{
+  width: 200px;
+}
+</style>
+  
